efa954433ca37e9d5a4cecfa310c79bdc394166e
Yu Li <liyu001989@gmail.com>
2016-12-15 16:19:38 +0800

    add HMAC.php

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# LARAVEL HMAC signature
+# LARAVEL signature
 
-
+想写一个关于消息签名的包，方便使用 hmac，或者 ras 对数据进行签名。
---
+++ src/HMAC.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Services;
+
+class HMAC
+{
+    /**
+     * key for sign.
+     *
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * sign algo.
+     *
+     * @var string
+     */
+    protected $algo;
+
+    public function __construct($key, $algo = 'sha1')
+    {
+        $this->key = $key;
+        $this->algo = $algo;
+    }
+
+    public function setKey($key)
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    public function setAlgo($key)
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    public function getAlgo()
+    {
+        return $this->algo;
+    }
+
+    /**
+     * make a sign for.
+     *
+     * @param array $params
+     *
+     * @return string
+     */
+    public function sign($signString)
+    {
+        return base64_encode(hash_hmac($this->algo, $signString, $this->key));
+    }
+
+    /**
+     * verify a sign.
+     *
+     * @param mixed $sign
+     * @param array $params
+     *
+     * @return bool
+     */
+    public function verify($sign, $signString)
+    {
+        return $sign == $this->sign($signString);
+    }
+}
